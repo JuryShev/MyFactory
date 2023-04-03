9561ef3004aa30acfa113e627bf8a9f0a710667b
Yury Shevr <shevrikukoyura@gmail.com>
2023-04-03 15:34:37 +0300

    Add style_.py

---
+++ client/GUI/qss/style_.py
@@ -0,0 +1,121 @@
+base_background_color="background-color: rgb(74, 80, 106)"
+text_error="""color: rgb(255, 129, 112);"""
+style_buttonBox_ONOFF="""background-color: rgb(204, 204, 204);"""
+style_asterisk_show="border-image: url(./GUI/icon/asterisk.png);"
+style_asterisk_hide="border-image: url();"
+style_LE_fields="""
+    border:none;
+    border-bottom: 1px solid rgb(16,240,207);
+    padding_bottom: 7px;
+    background-color: rgb(255, 255,255);
+    border-radius: 5px;
+
+"""
+style_list_connect_question="""
+QListWidget{
+    background-color: rgb(110, 120, 158,200);
+    border: none}
+
+QListView::item:hover{
+    background-color: rgb(11, 120, 158);}
+"""
+
+style_tab_question="""
+QTabWidget::pane {
+    border-top: 0px solid rgb(0, 0, 0);
+    border-left: 0px solid rgb(0, 0, 0);
+    border-right: 0px solid rgb(0, 0, 0);
+    border-bottom: 0px solid rgb(0, 0, 0);}
+
+QTabBar::tab {
+    background-color: rgb(74, 80, 106);
+    border: -1px solid lightgray;
+    padding: 10px;
+    color: rgb(230, 230, 230);}
+
+QTabBar::tab:selected {
+    background: rgb(245, 245, 245);
+    border: 1px solid lightgray;
+    border-bottom-color: rgb(0, 255, 127);
+    color: rgb(4, 4, 4);}
+"""
+
+PB_connect_style="""
+QPushButton{
+    background-color: rgb(74, 80, 106);
+    image: url(./GUI/icon/arrow.png);
+    border:None}
+    QPushButton:hover {image: url(./GUI/icon/arrow active.png);}
+"""
+
+float_menu_style="""
+    background-color: rgba(255, 255, 255, 0);
+"""
+
+LE_logo_question_style="""
+    border-bottom-color: rgb(102, 111, 147);background-color: rgb(102, 111, 147,0);
+"""
+LE_desc_question_style="""
+    color: rgb(230, 230, 230);
+"""
+LE_name_question_style="""
+    color: rgb(230, 230, 230);
+    border-bottom-color: rgb(102, 111, 147);
+"""
+
+question_form_connect_style="""
+.QFrame {background-color: rgb(102, 111, 147);
+    border: 1.3px solid black;
+    border-bottom-color: rgb(220, 220, 220);
+    border-right-color: rgb(102, 111, 147);
+    border-left-color: rgb(102, 111, 147);
+    border-top-color: rgb(102, 111, 147);}
+    
+.QFrame:hover{background-color: rgb(123, 135, 177)}"
+"""
+
+
+VL_inform_question_style = """
+border:None; background-color: rgb(102, 111, 147,0);"""
+
+PB_connect_question_style = """
+QPushButton{
+    background-color: rgb(132, 144, 190);
+    border:None;
+    border-radius: 5px;}
+
+QPushButton:hover{
+    background-color: rgb(154, 169, 222);
+    border:None;
+    border-radius: 5px;}
+
+QPushButton:pressed {
+     border-style: inset;
+     background-color: rgb(24, 33, 69);
+     color: rgb(250, 250, 250);
+     }"""
+
+PB_instr_question_style="""
+QPushButton{
+    background-color: rgb(230,230, 230);
+    border:None;
+    border-radius:8px;}
+    
+QPushButton:hover{
+    background-color: rgb(250,250, 250);
+    border:None;
+    border-radius:8px;}
+    
+QPushButton:pressed {
+    border-style: inset;
+    background-color: rgb(210, 210, 210);
+    }"""
+
+
+##########StyleDialogConnectQuest#########################################
+
+style_TB_open_json="""
+border:none;
+background-color: rgba(255, 255, 255, 0);
+border-radius: 5px;
+"""
